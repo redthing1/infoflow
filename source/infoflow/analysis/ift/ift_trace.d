32c67496da1706bbec6c9279778dee4026507f7c
redthing1 <redthing1@alt.icu>
2022-08-14 09:12:40 -0700

    add toHash

--- source/infoflow/analysis/ift/ift_trace.d
+++ source/infoflow/analysis/ift/ift_trace.d
@@ -348,6 +348,17 @@ template IFTAnalysis(TRegWord, TMemWord, TRegSet) {
                 long walk_commit_ix; // which commit to walk this back from
 
                 Nullable!IFTGraphNode parent;
+
+                size_t toHash() const @safe nothrow {
+                    size_t hash;
+                    
+                    hash += typeid(node).getHash(&node);
+                    hash += typeid(owner_commit_ix).getHash(&owner_commit_ix);
+                    hash += typeid(walk_commit_ix).getHash(&walk_commit_ix);
+                    hash += typeid(parent).getHash(&parent);
+
+                    return hash;
+                }
             }
 
             auto unvisited = DList!InfoNodeWalk();
