db758c9946ac2e26b6f657f830bb15cd26d1557e
redthing1 <redthing1@alt.icu>
2022-10-28 18:02:44 -0700

    wtf tree sit

--- tools/binned1/source/app.d
+++ tools/binned1/source/app.d
@@ -3,8 +3,6 @@ import std.file;
 import std.path;
 import std.array;
 import std.algorithm.comparison;
-import std.typecons;
-import std.format;
 import tsparse;
 
 import lang;
@@ -74,10 +72,6 @@ class FinderVisitor : ParseTreeVisitor {
 	}
 }
 
-struct TranslationResult {
-	string source;
-}
-
 void main(string[] args) {
 	// // read source file
 	// auto in_file = args[1];
@@ -96,9 +90,9 @@ void main(string[] args) {
 	auto queries = [
 		FinderVisitor.Query("struct_declaration", "struct InfoView"),
 		FinderVisitor.Query("struct_declaration", "struct InfoNode"),
-		FinderVisitor.Query("struct_declaration", "struct IFTGraphEdge"),
-		FinderVisitor.Query("struct_declaration", "struct IFTGraphNode"),
 	];
+
+	FinderVisitor.Result[FinderVisitor.Query] selected_results;
 
 	ParsedModule[] modules;
 
@@ -120,8 +114,6 @@ void main(string[] args) {
 		modules ~= parsed_module;
 	}
 
-	TranslationResult[FinderVisitor.Query] output_translations;
-
 	foreach (parsed_module; modules) {
 		auto finder = new FinderVisitor(parsed_module, queries);
 		parsed_module.traverse(finder);
@@ -138,110 +130,23 @@ void main(string[] args) {
 						.source[result.node.start_byte .. result.node.end_byte][0 .. min(20, result.node.end_byte - result
 								.node.start_byte)]);
 			}
+
+			// add first result to dict
+			selected_results[query] = finder.results[query][0];
 		}
+	}
 
-		// now use the first result of each
-		foreach (query; queries) {
-			if (query !in finder.results) {
-				continue;
-			}
-			auto first_result = finder.results[query][0];
-			writefln("creating translation for result: [%s], %s ...", first_result.node.kind, parsed_module
-					.source[first_result.node.start_byte .. first_result.node.end_byte][0 .. min(20, first_result
-							.node
-							.end_byte - first_result.node.start_byte)]);
-
-			// now create a translation result
-			auto translation_result = translate_node(parsed_module, first_result);
-			writefln("translation result: %s", translation_result);
-
-			output_translations[query] = translation_result;
+	// now we have the results, we can do stuff with them
+	// first log the results
+	writefln("selected results for %d queries", selected_results.length);
+	foreach (query; queries) {
+		writefln(" query: %s", query);
+		if (query !in selected_results) {
+			writefln("  no results");
+			continue;
 		}
-
+		auto result = selected_results[query];
+		// writefln("  result: [%s], %s ...", result.node.kind, result.node);
+		writefln("  result: [%s], %s ...", result.node, "TODO");
 	}
-}
-
-TranslationResult translate_node(ParsedModule pm, FinderVisitor.Result result) {
-	import d_tree_sitter;
-
-	// just print the node
-	writefln(" > translator input: [%s], %s ...", result.node.kind, pm
-			.source[result.node.start_byte .. result.node.end_byte][0 .. min(20, result.node.end_byte - result
-					.node.start_byte)]);
-
-	auto outsrc = appender!string();
-	auto root = result.node;
-	// auto cur = root.walk();
-
-	Nullable!Node kind_child_i(string kind, int ix) {
-		auto seen = 0;
-		for (auto i = 0; i < root.child_count; i++) {
-			auto maybe_child = root.child(i);
-			auto child = maybe_child.get;
-			if (child.kind == kind) {
-				if (seen == ix) {
-					// return child;
-					return Nullable!Node(child);
-				}
-				seen++;
-			}
-		}
-		return Nullable!Node.init;
-	}
-
-	Node[] get_kind_childs_deep(string kind) {
-		Node[] childs;
-		auto cur = root.walk();
-		
-		auto reached_end = false;
-		while (!reached_end) {
-			// visit the current node
-			if (cur.node.kind == kind) {
-				childs ~= cur.node;
-			}
-
-			if (cur.goto_first_child()) {
-				continue;
-			}
-			if (cur.goto_next_sibling()) {
-				continue;
-			}
-
-			auto retracing = true;
-			while (retracing) {
-				if (!cur.goto_parent()) {
-					retracing = false;
-					reached_end = true;
-				} else {
-					if (cur.goto_next_sibling()) {
-						retracing = false;
-					}
-				}
-			}
-		}
-
-		return childs;
-	}
-
-	auto node_str(Node node) {
-		return pm.source[node.start_byte .. node.end_byte];
-	}
-
-	// add the type declaration
-	outsrc ~= node_str(kind_child_i("struct", 0).get);
-	outsrc ~= " ";
-	outsrc ~= node_str(kind_child_i("identifier", 0).get);
-	outsrc ~= " {";
-
-	// add the fields
-	outsrc ~= "\n";
-	auto field_decls = get_kind_childs_deep("var_declarations");
-	foreach (field_decl; field_decls) {
-		outsrc ~= format("  %s\n", node_str(field_decl));
-	}
-
-	// close the struct
-	outsrc ~= "};";
-
-	return TranslationResult(outsrc.data);
 }
